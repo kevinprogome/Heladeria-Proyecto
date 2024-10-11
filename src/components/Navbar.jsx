f8eb69beecf184a80967fb74d62051271700ebd8
Jeisson Sanchez <jeissonesteban.sanchez@gmail.com>
2024-10-10 23:34:46 -0500

    Navbar finish

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,24 +2,43 @@ import "foundation-sites/dist/css/foundation.min.css";
 import "foundation-sites/dist/js/foundation.min.js";
 import "../styles/Navbar.css";
 import Logo from "../img/logo.png";
+import $ from "jquery";
+import { useEffect } from "react";
 
 const Navbar = () => {
+  useEffect(() => {
+    $(document).foundation();
+  }, []);
+
   return (
     <div>
-      <div className="top-bar">
+      <div
+        className="title-bar"
+        data-responsive-toggle="responsive-menu"
+        data-hide-for="large"
+      >
+        <button
+          className="menu-icon"
+          type="button"
+          data-toggle="responsive-menu"
+        ></button>
+        <div className="title-bar-title">Menu</div>
+      </div>
+
+      <div className="top-bar" id="responsive-menu">
         <div className="top-bar-left">
-          <ul className="menu">
+          <ul className="dropdown menu" data-dropdown-menu>
             <li className="menu-text">
               <a href="#">
                 <img src={Logo} alt="Logo Heladería" />
               </a>
             </li>
-            <li>
+            <li className="menu-left">
               <a className="link-12" href="#">
                 INICIO
               </a>
             </li>
-            <li>
+            <li className="menu-left">
               <a className="link-12" href="#">
                 <span className="material-symbols-outlined">g_translate</span>
               </a>
@@ -27,7 +46,7 @@ const Navbar = () => {
           </ul>
         </div>
         <div className="top-bar-right">
-          <ul className="menu">
+          <ul className="dropdown menu" data-dropdown-menu>
             <li>
               <a className="link-12" href="#">
                 PRODUCTO
