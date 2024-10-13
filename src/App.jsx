d6e2362f6e7d7111949581b3fef799bd3f7d61ea
Jeisson Sanchez <jeissonesteban.sanchez@gmail.com>
2024-10-13 16:50:54 -0500

    App1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import "foundation-sites/dist/css/foundation.min.css";
 import "foundation-sites/dist/js/foundation.min.js";
 import Navbar from "./components/Navbar.jsx";
+import Headers from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Headers />
+      <Footer />
     </div>
   );
 }
