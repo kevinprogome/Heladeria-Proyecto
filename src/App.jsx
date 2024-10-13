a4fa91bc14bf8a0be2522b4bc800aa5a6fac5c85
JuanVidarte <juanvidartedussan@gmail.com>
2024-10-13 16:46:10 -0500

    Cambios Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "foundation-sites/dist/css/foundation.min.css";
 import "foundation-sites/dist/js/foundation.min.js";
 import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <Navbar/>
+      <Footer/>
     </div>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,81 +1,67 @@
 import "foundation-sites/dist/css/foundation.min.css";
 import "foundation-sites/dist/js/foundation.min.js";
-import "../styles/Footer.css"
+import "../styles/Footer.css";
+import $ from "jquery";
+import { useEffect } from "react";
 
 const Footer = () => {
+  useEffect(() => {
+    // Inicializa Foundation una vez que el DOM esté cargado
+    $(document).foundation();
+  }, []);
 
   return (
-      <footer class="marketing-site-footer">
-    <div class="row medium-unstack">
-      <div class="medium-4 columns">
-        <h4 class="marketing-site-footer-name">Yeti Snowcone</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita dolorem accusantium architecto id quidem, itaque nesciunt quam ducimus atque.</p>
-        <ul class="menu marketing-site-footer-menu-social simple">
-          <li><a href="#"><i class="fa fa-youtube-square" aria-hidden="true"></i></a></li>
-           <li><a href="#"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
-           <li><a href="#"><i class="fa fa-twitter-square" aria-hidden="true"></i></a></li>
-        </ul>
-      </div>
-      <div class="medium-4 columns">
-         <h4 class="marketing-site-footer-title">Contact Info</h4>
-        <div class="marketing-site-footer-block">
-          <i class="fa fa-map-marker" aria-hidden="true"></i>
-          <p>100 W Rincon<br>San Francisco, CA 94015</p>
-        </div>
-        <div class="marketing-site-footer-block">
-          <i class="fa fa-phone" aria-hidden="true"></i>
-          <p>1 (800) 555-5555</p>
-        </div>
-        <div class="marketing-site-footer-block">
-          <i class="fa fa-envelope-o" aria-hidden="true"></i>
-          <p>yetirules@fakeemail.com</p>
-        </div>
-      </div>
-      <div class="medium-4 columns">
-        <h4 class="marketing-site-footer-title">Instagram</h4>
-        <div class="row small-up-3">
-          <div class="column column-block">
-            <img src="https://placehold.it/75" alt="" />
-          </div>
-          <div class="column column-block">
-            <img src="https://placehold.it/75" alt="" />
-          </div>
-          <div class="column column-block">
-            <img src="https://placehold.it/75" alt="" />
-          </div>
-          <div class="column column-block">
-            <img src="https://placehold.it/75" alt="" />
-          </div>
-          <div class="column column-block">
-            <img src="https://placehold.it/75" alt="" />
-          </div>
-          <div class="column column-block">
-            <img src="https://placehold.it/75" alt="" />
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="marketing-site-footer-bottom">
-      <div class="row large-unstack align-middle">
-        <div class="column">
-          <p>&copy; 2017 No rights reserved</p>
-        </div>
-        <div class="column">
-          <ul class="menu marketing-site-footer-bottom-links">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Works</a></li>
-            <li><a href="#">News</a></li>
-            <li><a href="#">Contact</a></li>
+    <footer className="marketing-site-footer grid-container full">
+      <div className="grid-x grid-margin-x">
+        {/* Primera columna */}
+        <div className="cell medium-6">
+          <h4 className="marketing-site-footer-name">Yeti Snowcone</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita
+            dolorem accusantium architecto id quidem, itaque nesciunt quam
+            ducimus atque.
+          </p>
+          <ul className="menu marketing-site-footer-menu-social simple">
+            <li>
+              <a href="#">
+                <i className="fa fa-youtube-square" aria-hidden="true"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="fa fa-facebook-square" aria-hidden="true"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="fa fa-twitter-square" aria-hidden="true"></i>
+              </a>
+            </li>
           </ul>
         </div>
+        {/* Segunda columna */}
+        <div className="cell medium-6">
+          <h4 className="marketing-site-footer-title">Contact Info</h4>
+          <div className="marketing-site-footer-block">
+            <i className="fa fa-map-marker" aria-hidden="true"></i>
+            <p>
+              100 W Rincon
+              <br />
+              San Francisco, CA 94015
+            </p>
+          </div>
+          <div className="marketing-site-footer-block">
+            <i className="fa fa-phone" aria-hidden="true"></i>
+            <p>1 (800) 555-5555</p>
+          </div>
+          <div className="marketing-site-footer-block">
+            <i className="fa fa-envelope-o" aria-hidden="true"></i>
+            <p>yetirules@fakeemail.com</p>
+          </div>
+        </div>
       </div>
-    </div>
-</footer>
+    </footer>
+  );
+};
 
-
-  )
-}
-
-export default Footer
+export default Footer;
